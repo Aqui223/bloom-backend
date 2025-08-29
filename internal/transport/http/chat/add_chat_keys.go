b07657a7b26dcd6035862265d8071f32d74401ee
pip64 <pip64b@icloud.com>
2025-08-29 16:17:24 +0700

    Add keys length check to add chat keys controller

--- internal/transport/http/chat/add_chat_keys.go
+++ internal/transport/http/chat/add_chat_keys.go
@@ -1,6 +1,10 @@
 package chat
 
 import (
+	"encoding/base64"
+	"errors"
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/slipe-fun/skid-backend/internal/transport/http"
 )
@@ -34,6 +38,30 @@ func (h *ChatHandler) AddChatKeys(c *fiber.Ctx) error {
 
 	if req.KyberPublicKey == "" || req.EcdhPublicKey == "" || req.EdPublicKey == "" {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "u need to specify all keys"})
+	}
+
+	kyberKey, err := base64.StdEncoding.DecodeString(req.KyberPublicKey)
+	if err != nil {
+		return errors.New("invalid base64 for Kyber key")
+	}
+	ecdhKey, err := base64.StdEncoding.DecodeString(req.EcdhPublicKey)
+	if err != nil {
+		return errors.New("invalid base64 for ECDH key")
+	}
+	edKey, err := base64.StdEncoding.DecodeString(req.EdPublicKey)
+	if err != nil {
+		return errors.New("invalid base64 for Ed25519 key")
+	}
+
+	if len(kyberKey) != 1184 {
+		return errors.New("invalid Kyber key length")
+	}
+	fmt.Print(len(ecdhKey))
+	if len(ecdhKey) != 44 {
+		return errors.New("invalid ECDH key length")
+	}
+	if len(edKey) != 44 {
+		return errors.New("invalid Ed25519 key length")
 	}
 
 	chat, err := h.chatApp.GetChatById(token, chatId)
