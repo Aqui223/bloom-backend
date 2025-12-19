7859249a84e3bcb9966d87b71052c1db659db3a9
pip64 <76626915+pip64@users.noreply.github.com>
2025-12-20 00:29:28 +0700

    .

--- internal/domain/socket_keys.go
+++ internal/domain/socket_keys.go
@@ -2,7 +2,7 @@ package domain
 
 type SocketKeys struct {
 	ChatID         int    `json:"chat_id"`
-	KyberPublicKey string `json:"kyberPublicKey"`
-	EcdhPublicKey  string `json:"ecdhPublicKey"`
-	EdPublicKey    string `json:"edPublicKey"`
+	KyberPublicKey string `json:"kyber_public_key"`
+	EcdhPublicKey  string `json:"ecdh_public_key"`
+	EdPublicKey    string `json:"ed_public_key"`
 }
