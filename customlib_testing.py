ec12e29b885d5d7fdf1aa9b0ce587443e8fb0e10
Aquiren <aquiren@proton.me>
2026-02-27 23:05:35 +0500

    Refactor list_missing to include quiet parameter
    Updated list_missing function to accept a quiet parameter and modified route checking logic.

--- customlib_testing.py
+++ customlib_testing.py
@@ -1,8 +1,8 @@
 import requests
+from tests_config import *
 
 # Could use a lib like unittests but not this time
 
-server = "http://127.0.0.1:8080"
 show_success = True
 
 def printfail(fail_text: str, route: str, fail_type: str = "UNKNOWN"):
@@ -75,7 +75,7 @@ def g(route: str, result="error", request_data: dict = None, status_code: int = 
     tests.append(Test(route=route, result=result, request_data=request_data, s_code=status_code))
     test_routes.update((route,))
 
-def list_missing():
+def list_missing(quiet: bool = False):
     with open("cmd/api/main.go") as file:
         go_code = file.read()
 
@@ -84,15 +84,27 @@ def list_missing():
     go_code = go_code[start:end]
 
     go_lines = go_code.split("\n")
+    cc_missing_tests = [] # CurrentlyConstructed
+    cc_methods = []
     for line in go_lines:
         if len(line.strip()) == 0: continue
 
         start_route = line.index('t("/')
         end_route = line.index('", ')
         route = (line[start_route+3:end_route])
-        if route not in test_routes:
+        if route not in test_routes and not quiet:
             print("Not added:", route)
+        if line[start_route-4:start_route+1]==".Post":
+            method = "POST"
+        elif line[start_route-4:start_route+1]=="p.Get":
+            method = "GET"
+        else:
+            print(line)
+            raise SyntaxError("What happened here?? Can't detect the method")
         
+        cc_missing_tests.append((route, method))
+        
+    return cc_missing_tests
 
 def run_tests():
     [test() for test in tests]
